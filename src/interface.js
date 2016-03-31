cf847c72f51fed9f4e74100af484ab35e673041a
olmesm <olmesm@gmail.com>
2016-03-31 17:52:40 +0100

    added decrease function

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,15 @@
 $( document ).ready(function() {
 
   var thermostat = new Thermostat();
-  $( "#temperature" ).html( thermostat.temperature() )
+  $( "#temperature" ).html( thermostat.temperature() );
 
   $( "#temperature-increase" ).on( "click", function( event ) {
     thermostat.increase();
+    $( "#temperature" ).html( thermostat.temperature() );
+  });
+
+  $( "#temperature-decrease" ).on( "click", function( event ) {
+    thermostat.decrease();
     $( "#temperature" ).html( thermostat.temperature() );
   });
 
